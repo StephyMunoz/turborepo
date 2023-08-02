05ea3e8cb206d844097df5d2f065f871b0f6808c
StephyMunoz <stephanie.munoz.live@gmail.com>
2023-08-02 15:06:26 -0500

    Change the button

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
+  return <button onClick={() => alert("Button to prove turborepo")}>Boop</button>;
 };
